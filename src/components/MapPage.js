6815aea8e7b45ef62b08104bd157a65af5ea745c
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-11-01 23:33:45 +0800

    disable map page

--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow, } from "react-google-maps";
 
 class MapPage extends Component {
 
@@ -21,34 +20,34 @@ class MapPage extends Component {
     }
 
     
-    render() {
-        const MapWithAMarker = withScriptjs(withGoogleMap(props =>
-            <GoogleMap
-                defaultZoom={8}
-                defaultCenter={{ lat: -34.397, lng: 150.644 }}
-            >
-                <Marker
-                    draggable = {true}
-                    onDragEnd = {this.onDragEnd}
-                    position={{ lat: -34.397, lng: 150.644 }}
-                >
-                    <InfoWindow>
-                        <div>
-                            Sample Location
-                        </div>
-                    </InfoWindow>
-                </Marker>
-            </GoogleMap>
-        ));
-        return (
-            <MapWithAMarker
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfcBAIc0tdsDnV-UZqPxC1nyYBupfpPHE&v=3.exp&libraries=geometry,drawing,places"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `400px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
-        );
-    }
+    // render() {
+    //     const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+    //         <GoogleMap
+    //             defaultZoom={8}
+    //             defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    //         >
+    //             <Marker
+    //                 draggable = {true}
+    //                 onDragEnd = {this.onDragEnd}
+    //                 position={{ lat: -34.397, lng: 150.644 }}
+    //             >
+    //                 <InfoWindow>
+    //                     <div>
+    //                         Sample Location
+    //                     </div>
+    //                 </InfoWindow>
+    //             </Marker>
+    //         </GoogleMap>
+    //     ));
+    //     return (
+    //         <MapWithAMarker
+    //             googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfcBAIc0tdsDnV-UZqPxC1nyYBupfpPHE&v=3.exp&libraries=geometry,drawing,places"
+    //             loadingElement={<div style={{ height: `100%` }} />}
+    //             containerElement={<div style={{ height: `400px` }} />}
+    //             mapElement={<div style={{ height: `100%` }} />}
+    //         />
+    //     );
+    // }
 }
 
 export default MapPage;
