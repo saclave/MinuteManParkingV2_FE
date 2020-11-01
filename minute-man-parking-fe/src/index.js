6a0bccf1fabed32e56570bfb4241f8f6c9e9152e
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-11-01 23:55:28 +0800

    reset package

--- minute-man-parking-fe/src/index.js
+++ minute-man-parking-fe/src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
-import reducers from './reducers';
-import { Provider } from 'react-redux';
-
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
--- src/actions.js
+++
@@ -1,3 +0,0 @@
-export const getAccount = accounts => {
-    return { type: "GET_ACCOUNT", payload: accounts };
-}
